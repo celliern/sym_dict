6f7bc950c54caba7e4aed316da8146043734d698
nicolas <nicolas@localhost.localdomain>
2017-01-30 13:52:16 +0100

    init repo

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+setup(
+    name='sym_dict',
+    # la version du code
+    version="0.1.0",
+    # Liste les packages à insérer dans la distribution
+    packages=find_packages(),
+
+    author="Nicolas CELLIER",
+    author_email="contact@nicolas-cellier.net",
+    description="Dictionary based tool using "
+                "symbolic relation to deduce values",
+
+    license='MIT',
+
+    # long_description=open('README.md').read(),
+
+    # ext_modules=ext_modules,
+    # Active la prise en compte du fichier MANIFEST.in
+    include_package_data=True,
+
+    # Une url qui pointe vers la page officielle de votre lib
+    url='http://github.com/celliern/sym_dict',
+
+    # Il est d'usage de mettre quelques metadata à propos de sa lib
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers.
+
+    classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
