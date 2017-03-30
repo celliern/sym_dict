c824ef4753c2a0d52d8bf48bd9ff346e388dc16f
Nicolas Cellier <contact@nicolas-cellier.net>
2017-03-30 16:19:59 +0200

    clean import and requirements

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 setup(
     name='sym_dict',
     # la version du code
-    version="0.2",
+    version="0.2.0",
     # Liste les packages à insérer dans la distribution
     packages=find_packages(),
 
